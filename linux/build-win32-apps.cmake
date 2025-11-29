c1e2dc77faaf9d847c2699a6614d04033518e6d4
Joseph Artsimovich <joseph.artsimovich@gmx.com>
2025-11-29 16:20:43 +0200

    Install the Flutter SDK through snap
    Installing Flutter from snap means we can't control the version we are installing, but on the
    positive side, the version from snap comes with older glib / gtk / whatever, so the binaries
    it builds are able to run on older distros.
    
    Unfortunately, the Flutter from snap brings an old version of CMake with it. We build a few C/C++
    utilities in addition to our Flutter app and we need CMake 3.28 at least. To overcome this issue,
    we download and extract a newer version of CMake and overlay it on top of the Flutter installation
    from snap.
    
    Yet another complication coming from the snap version of Flutter is that it sets various environment
    variables such as LDFLAGS that break our win32-apps build. We solve that by explicitly unsetting those
    variables in ExternalProject_Add().

--- linux/build-win32-apps.cmake
+++ linux/build-win32-apps.cmake
@@ -3,6 +3,17 @@ include(ExternalProject)
 set(prefix_dir "${PROJECT_BINARY_DIR}/win32-apps")
 set(install_dir "${prefix_dir}/instdir")
 
+# When Flutter SDK is installed from snap, we are run with some environment variables set,
+# which interfere with our win32 cross-build. This command unsets the problematic environment
+# variables.
+set(
+    unset_env_vars_command_wrapper
+    ${CMAKE_COMMAND} -E env
+    --unset=CPLUS_INCLUDE_PATH
+    --unset=LIBRARY_PATH
+    --unset=LDFLAGS
+)
+
 ExternalProject_Add(
     win32-apps
     BUILD_ALWAYS TRUE
@@ -12,11 +23,17 @@ ExternalProject_Add(
     DOWNLOAD_COMMAND ""
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
-    CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
-    "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
-    "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/../win32-apps/cmake/mingw-w64-x86.cmake"
 
-    INSTALL_COMMAND ${CMAKE_COMMAND} --install . --config Release $<$<NOT:$<CONFIG:Debug>>:--strip>
+    CONFIGURE_COMMAND ${unset_env_vars_command_wrapper} ${CMAKE_COMMAND}
+    -DCMAKE_BUILD_TYPE=Release "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
+    "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/../win32-apps/cmake/mingw-w64-x86.cmake"
+    <SOURCE_DIR>
+    
+    BUILD_COMMAND ${unset_env_vars_command_wrapper} ${CMAKE_COMMAND}
+    --build . --config Release
+
+    INSTALL_COMMAND ${unset_env_vars_command_wrapper} ${CMAKE_COMMAND}
+    --install . --config Release $<$<NOT:$<CONFIG:Debug>>:--strip>
 
     # Adding that COMMAND_EXPAND_LISTS is a dirty hack to pass it to the underlying add_custom_target()
     # in order to avoid passing an empty argument to cmake --install in a debug configuration.    
